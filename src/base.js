87e1fa806a602d6e688d41955f5761596a835d2f
Grimstir <xbio.oo@gmail.com>
2018-04-24 17:48:18 +0800

    Removed API Key

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyC2N7y9tDlrd1wbIOenNxC3RGk1rj6lCyg",
+  apiKey: process.env.BASE_APIKEY,
   authDomain: "catch-of-the-day-sy-165fb.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-sy-165fb.firebaseio.com",
   projectId: "catch-of-the-day-sy-165fb",
